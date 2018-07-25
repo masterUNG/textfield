a829eabb37a2455dac7e200e7c2f832c1ea5998d
MasterUNG <phrombutr@gmail.com>
2018-07-26 04:48:50 +0700

    update 26July

--- lib/loginPage.dart
+++ lib/loginPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'user.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -6,6 +7,8 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final User user = new User();
+
   @override
   Widget build(BuildContext context) {
     final TextField _txtUserName = new TextField(
@@ -16,16 +19,26 @@ class _LoginPageState extends State<LoginPage> {
       ),
       keyboardType: TextInputType.text,
       autocorrect: false,
+      onChanged: (text) {
+        setState(() {
+          this.user.userNameString = text;
+        });
+      },
     );
 
     final TextField _txtEmail = new TextField(
       decoration: new InputDecoration(
-          hintText: 'Email',
+        hintText: 'Email',
         contentPadding: new EdgeInsets.all(10.0),
         border: InputBorder.none,
       ),
       keyboardType: TextInputType.text,
       autocorrect: false,
+      onChanged: (text) {
+        setState(() {
+          this.user.emailString = text;
+        });
+      },
     );
 
     final TextField _txtPassword = new TextField(
@@ -37,6 +50,11 @@ class _LoginPageState extends State<LoginPage> {
       keyboardType: TextInputType.text,
       autocorrect: false,
       obscureText: true,
+        onChanged: (text){
+        setState(() {
+          this.user.passwordString = text;
+        });
+        },
     );
 
     return new Scaffold(
@@ -44,9 +62,16 @@ class _LoginPageState extends State<LoginPage> {
         title: new Text("Login Page AppBar"),
       ),
       body: new Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           new Container(
-            margin: new EdgeInsets.only(left: 20.0, right: 20.0, bottom: 10.0, top: 20.0),
+            width: 200.0,
+            height: 200.0,
+            child: new Image.asset('images/masterung_round.png'),
+          ),
+          new Container(
+            margin: new EdgeInsets.only(
+                left: 20.0, right: 20.0, bottom: 10.0, top: 20.0),
             decoration: new BoxDecoration(
                 color: Color.fromARGB(255, 240, 240, 240),
                 border: new Border.all(width: 1.2, color: Colors.black12),
@@ -55,33 +80,40 @@ class _LoginPageState extends State<LoginPage> {
             child: _txtUserName,
           ),
           new Container(
-          margin: new EdgeInsets.only(left: 20.0, right: 20.0),
+            margin: new EdgeInsets.only(left: 20.0, right: 20.0),
             decoration: new BoxDecoration(
-              color: Color.fromARGB(255, 240, 240, 240),
-              border: new Border.all(width: 1.2, color: Colors.black12),
-              borderRadius:
-              const BorderRadius.all(const Radius.circular(25.0))),
+                color: Color.fromARGB(255, 240, 240, 240),
+                border: new Border.all(width: 1.2, color: Colors.black12),
+                borderRadius:
+                    const BorderRadius.all(const Radius.circular(25.0))),
             child: _txtEmail,
           ),
           new Container(
-          margin: new EdgeInsets.only(left: 20.0, right: 20.0, top: 10.0),
+            margin: new EdgeInsets.only(left: 20.0, right: 20.0, top: 10.0),
             decoration: new BoxDecoration(
-              color: Color.fromARGB(255, 240, 240, 240),
-              border: new Border.all(width: 1.2, color: Colors.black12),
-              borderRadius: new BorderRadius.all(const Radius.circular(25.0))
-            ),
+                color: Color.fromARGB(255, 240, 240, 240),
+                border: new Border.all(width: 1.2, color: Colors.black12),
+                borderRadius:
+                    new BorderRadius.all(const Radius.circular(25.0))),
             child: _txtPassword,
           ),
-          new Row(
-            children: <Widget>[
-              new RaisedButton(
+          new Container(
+            margin: EdgeInsets.only(left: 20.0, right: 20.0, top: 10.0),
+            child: new Row(
+              children: <Widget>[
+                new Expanded(
+                    child: new RaisedButton(
+                  child: new Text("Login"),
                   textColor: Colors.white,
                   color: Colors.blue,
-                  child: new Text("Login"),
-                  onPressed: (){
-
-              }),
-            ],
+                  onPressed: () {
+                    print('userNameString ==> ${user.userNameString}');
+                    print('emailString ==> ${user.emailString}');
+                    print('passwordString ==> ${user.passwordString}');
+                  },
+                )),
+              ],
+            ),
           ),
         ],
       ),
